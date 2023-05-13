b30aaa319c3778b37c4dc0408efa1c541c646763
PaolaUribe88 <paolauribe88@gmail.com>
2023-05-12 22:54:59 -0400

    Creacion de otro formulario, con un objeto nuevo libro

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,15 +23,35 @@ var persona ={
 
 
 };//instruccion
-function asignarDatosAlObjeto(unNombre, unApellido, unEmail, unTelefono){
+//OBJETO LIBRO
+var libro ={
+    nombreLibro:'',
+    autorLibro:'',
+    editorialLibro:'',
+    paginasLibro:'',
+    
+    mostrarDatosLibros: function(){
+        let datoLibro =`El titulo del libro es ${this.nombreLibro}, el autor es ${this.autorLibro}, su editorial es ${this.editorialLibro} y su cantidad de paginas son ${this.paginasLibro}`
+        return datoLibro;
+    },
+
+}
+function asignarDatosAlObjeto(unNombre, unApellido, unEmail, unTelefono){//ASIGNACION DE LA PERSONA
     persona.nombre = unNombre;
     persona.apellido = unApellido;
     persona.email = unEmail;
     persona.telefono = unTelefono;
 
 }
+function asignarDatosAlObjetoLibro(unNombre, unAutor, unaEditorial, unasPaginas){//ASIGNACION DEL LIBRO
+    libro.nombreLibro = unNombre;
+    libro.autorLibro = unAutor;
+    libro.editorialLibro = unaEditorial;
+    libro.paginasLibro = unasPaginas;
+}
 
-function atraparDatosF(){
+
+function atraparDatosF(){ //ATRAPA DATOS DEL FORMULARIO
     // alert ('funciona el boton');
     //creamos las variables de los inputs del formulario
     let elNombre = document.getElementById('nombre').value;
@@ -44,14 +64,32 @@ function atraparDatosF(){
     asignarDatosAlObjeto(elNombre, elApellido,elEmail,elCelular);
 
 }
-function presentarInformacion(){
+function atraparDatosLibro(){ //ATRAPA DATOS DEL LIBRO
+    // alert ('funciona el boton');
+    //creamos las variables de los inputs del formulario
+    let elNombreLibro = document.getElementById('nombreLibro').value;
+    let elAutorLibro = document.getElementById('autorLbro').value;
+    let laEditorial = document.getElementById('editorial').value;
+    let lasPaginas = document.getElementById('txtPag').value;
+
+    //ASIGNAR DATOS ATRAPADOS DEL FORM AL OBJETO
+
+    asignarDatosAlObjetoLibro(elNombreLibro, elAutorLibro, laEditorial, lasPaginas);
+
+}
+function presentarInformacion(){ //PRESENTA INFORMACION PERSONA
     let elParrafoSaludo = document.getElementById('parrafoSaludo');
     elParrafoSaludo.innerText= persona.saludar();
 
     let elParrafoContacto = document.getElementById('parrafoContacto');
     elParrafoContacto.innerText = persona.proporcionarDatosContacto();
-}
+    
 
+}
+function presentarInformacionLibro(){// PRESENTA INFORMACION LIBRO
+    let elParrafoLibro = document.getElementById('parrafoLibro');
+    elParrafoLibro.innerText= libro.mostrarDatosLibros();
+}
 function asignarEventos(){
     //creamos una variable con el que manipulamos el elemento del dom llamado btnEnviar
     let elBoton = document.getElementById('btnEnviar');
@@ -60,4 +98,10 @@ function asignarEventos(){
 
     let elBotonMostra = document.getElementById('btnMostrarDatos');
     elBotonMostra.addEventListener('click', presentarInformacion);
+
+    let elBotonLibro = document.getElementById('btnEnviarL');
+    elBotonLibro.addEventListener('click', atraparDatosLibro );
+
+    let elBotonMostrarLibro = document.getElementById('btnMostrarDatosLibro');
+    elBotonMostrarLibro.addEventListener('click',presentarInformacionLibro )
 }
